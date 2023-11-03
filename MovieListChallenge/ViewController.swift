1795bd910e036a5934595598ddb0e9a2e825d88d
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2023-11-03 20:02:43 +0300

    Initial commit

---
+++ MovieListChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListChallenge
+//
+//  Created by Sezgin Çiftci on 3.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
